d71cf1008aa7bea98116c6308367f4d6626a0dfe
aporve <aporvep@gmail.com>
2021-06-08 21:27:41 +0530

    Changed Nudge Notification position

--- custom.js
+++ custom.js
@@ -1,6 +1,6 @@
-document.getElementById('ym-notification').style.bottom = '198p';
+document.getElementById('ym-notification').style.bottom = '198px';
 var observer = new MutationObserver(function(mutations) {
-    document.getElementById('ym-notification').style.bottom = '198p'; 
+    document.getElementById('ym-notification').style.bottom = '198px'; 
 });
 var target = document.getElementById('ym-notification');
 observer.observe(target, { attributes : true, attributeFilter : ['style'] });
