46b47564cc931074ff073ebfb04bfd4fc1411a2b
aporve <aporvep@gmail.com>
2021-06-22 10:38:51 +0530

    changed height

--- custom.js
+++ custom.js
@@ -1,6 +1,6 @@
-document.getElementById('ym-notification').style.bottom = '188px';
+document.getElementById('ym-notification').style.bottom = '160px';
 var observer = new MutationObserver(function(mutations) {
-    document.getElementById('ym-notification').style.bottom = '188px'; 
+    document.getElementById('ym-notification').style.bottom = '160px'; 
 });
 var target = document.getElementById('ym-notification');
 observer.observe(target, { attributes : true, attributeFilter : ['style'] });
